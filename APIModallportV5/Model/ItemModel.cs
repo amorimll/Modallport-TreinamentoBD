cdaa7d5a42ca9a67dbd338c968e99aa8d14dec62
caicrs <caicrs.contact@gmail.com>
2023-06-15 11:26:00 -0300

    Models - New models for vistorias realizadas

--- APIModallportV5/Model/ItemModel.cs
+++ APIModallportV5/Model/ItemModel.cs
@@ -16,4 +16,32 @@ namespace APIModallportV5.Model
         //public List<OpcoesListModel> opcaoModels { get; set; }
         //public List<RespostasListModel> respostaModels { get; set; }
     }
+
+    public class ItemRespondidoModel
+    {
+        public string Descricao { get; set; }
+        public int Ordem { get; set; }
+        public int Tipo { get; set; }
+        public string[] Opcoes { get; set; }
+        public string[] Resposta { get; set; }
+    }
+
+    public class ItemModelAndOption
+    {
+        public int IdOpcao { get; set; }
+        public string Opcao { get; set; }
+        public int IdItem { get; set; }
+        public int IdItem_1 { get; set; }
+        public string Descricao { get; set; }
+        public int Ordem { get; set; }
+        public int Tipo { get; set; }
+        public string Ativo { get; set; }
+        public DateTime DataDeCadastro { get; set; }
+        public DateTime DhAlteracao { get; set; }
+        public DateTime DataDeCadastro_1 { get; set; }
+        public DateTime DhAlteracao_1 { get; set; }
+        public int IdVistoria { get; set; }
+        //public List<OpcoesListModel> opcaoModels { get; set; }
+        //public List<RespostasListModel> respostaModels { get; set; }
+    }
 }

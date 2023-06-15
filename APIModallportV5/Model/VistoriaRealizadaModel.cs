eb1d4077c2aa1b49c1ee6dbb682225e4d0e5e03a
caicrs <caicrs.contact@gmail.com>
2023-06-15 11:26:50 -0300

    Models - VistoriaRealizada model created

---
+++ APIModallportV5/Model/VistoriaRealizadaModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http.Json;
+using System;
+using System.Collections.Generic;
+
+namespace APIModallportV5.Model
+{
+    public class VistoriaRealizadaModel
+    {
+        public int IdVistoriaRealizada { get; set; }
+        public string Descricao { get; set; }
+        public object Itens { get; set; }
+        public int Processo { get; set; }
+        public string Ativo { get; set; }
+        public DateTime DataDeCadastro { get; set; }
+        public DateTime DhAlteracao { get; set; }
+    }
+
+    public class CreateVistoriaRealizadaModel
+    {
+        public int IdVistoriaRealizada { get; set; }
+        public string Descricao { get; set; }
+        public object Itens { get; set; }
+        public int Processo { get; set; }
+        public string Ativo { get; set; }
+        public DateTime DataDeCadastro { get; set; }
+        public DateTime DhAlteracao { get; set; }
+    }
+}

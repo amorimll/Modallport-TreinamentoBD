240498427fc148f697de3fc047cfb791bd4b7987
caicrs <caicrs.contact@gmail.com>
2023-06-15 11:25:12 -0300

    Dao - Code for by id methods

--- APIModallportV5/Dao/DaoVistorias.cs
+++ APIModallportV5/Dao/DaoVistorias.cs
@@ -5,6 +5,7 @@ using Oracle.ManagedDataAccess.Types;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 
 namespace APIModallportV5.Dao
 {
@@ -63,6 +64,166 @@ namespace APIModallportV5.Dao
                 throw new Exception(ex.Message);
             }
         }
+
+        public List<VistoriaModel> ListaVistoriasProcesso(int idProcesso)
+        {
+            try
+            {
+                _connection.Open();
+
+                var vistorias = new List<VistoriaModel>();
+
+                using (var command = _connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT IdVistoria,  Descricao, Processo, Ativo, DataDeCadastro, DhAlteracao FROM Vistorias WHERE Ativo = 'S' AND Processo = :IdProcesso";
+                    // Bind the value to the parameter
+                    command.Parameters.Add(":IdProcesso", idProcesso);
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var vistoria = new VistoriaModel
+                            {
+                                IdVistoria = reader.GetInt32(reader.GetOrdinal("IdVistoria")),
+                                Descricao = reader.GetString(reader.GetOrdinal("Descricao")),
+                                Processo = reader.GetInt32(reader.GetOrdinal("Processo")),
+                                Ativo = reader.GetString(reader.GetOrdinal("Ativo")),
+                                DataDeCadastro = reader.GetDateTime(reader.GetOrdinal("DataDeCadastro")),
+                                DhAlteracao = reader.GetDateTime(reader.GetOrdinal("DhAlteracao"))
+                            };
+
+                            vistorias.Add(vistoria);
+                        }
+                    }
+                }
+
+                _connection.Close();
+                _logService.PerformOperation("GET", "Dados de VISTORIAS apartir do id do processo retornados.");
+
+                return vistorias;
+
+            }
+            catch (Exception ex)
+            {
+                _logService.PerformOperation("GET", $"{ex.Message}");
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<VistoriaModelAndItems> ListaVistoriasItens(int idVistoria)
+        {
+            try
+            {
+                _connection.Open();
+
+                var vistorias = new List<VistoriaModelAndItems>();
+                var itens = new List<ItemModel>();
+                var itensAndOptions = new List<ItemModelAndOption>();
+
+                // Itens list
+                using (var command = _connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT IdItem, Descricao, Ordem, Tipo, Ativo, IdVistoria, DataDeCadastro, DhAlteracao FROM Itens WHERE Ativo = 'S' AND idVistoria = :idVistoria";
+                    command.Parameters.Add(":IdVistoria", idVistoria);
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var item = new ItemModel
+                            {
+                                IdItem = reader.GetInt32(reader.GetOrdinal("IdItem")),
+                                Descricao = reader.GetString(reader.GetOrdinal("Descricao")),
+                                Ordem = reader.GetInt32(reader.GetOrdinal("Ordem")),
+                                Tipo = reader.GetInt32(reader.GetOrdinal("Tipo")),
+                                Ativo = reader.GetString(reader.GetOrdinal("Ativo")),
+                                IdVistoria = reader.GetInt32(reader.GetOrdinal("IdVistoria")),
+                                DataDeCadastro = reader.GetDateTime(reader.GetOrdinal("DataDeCadastro")),
+                                DhAlteracao = reader.GetDateTime(reader.GetOrdinal("DhAlteracao"))
+                            };
+
+                            itens.Add(item);
+                        }
+                    }
+                }
+
+                // var idVistoriaArray = itens.Split(',').Select(int.Parse).ToArray();
+                var idVistoriaArray = itens.ToArray().Select(item => item.IdItem).ToArray();
+                string joinedString = idVistoriaArray.Length > 0 ? string.Join(",", idVistoriaArray ) : "null";
+                Console.WriteLine(joinedString);
+
+                if (joinedString != "null")
+                {
+                    // Itens and Options list
+                    using (var command = _connection.CreateCommand())
+                    {
+                        command.CommandText = $"SELECT * FROM OpcoesItens JOIN itens ON Itens.iditem = OpcoesItens.Iditem WHERE OpcoesItens.Iditem IN ({joinedString})";
+                        using (var reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                var itemOptions = new ItemModelAndOption
+                                {
+                                    IdItem = reader.GetInt32(reader.GetOrdinal("IdItem")),
+                                    IdOpcao = reader.GetInt32(reader.GetOrdinal("IdOpcao")),
+                                    Opcao = reader.GetString(reader.GetOrdinal("Opcao")),
+                                    Descricao = reader.GetString(reader.GetOrdinal("Descricao")),
+                                    Ordem = reader.GetInt32(reader.GetOrdinal("Ordem")),
+                                    Tipo = reader.GetInt32(reader.GetOrdinal("Tipo")),
+                                    Ativo = reader.GetString(reader.GetOrdinal("Ativo")),
+                                    IdVistoria = reader.GetInt32(reader.GetOrdinal("IdVistoria")),
+                                    DataDeCadastro = reader.GetDateTime(reader.GetOrdinal("DataDeCadastro")),
+                                    DhAlteracao = reader.GetDateTime(reader.GetOrdinal("DhAlteracao"))
+                                };
+
+                                itensAndOptions.Add(itemOptions);
+                            }
+                        }
+                    }
+                }
+
+
+                using (var command = _connection.CreateCommand())
+                {
+                    command.Parameters.Add(":IdVistoria", idVistoria);
+                    command.CommandText = "SELECT IdVistoria, Descricao, Processo, Ativo, DataDeCadastro, DhAlteracao FROM Vistorias WHERE Ativo = 'S' AND idVistoria = :idVistoria";
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var vistoria = new VistoriaModelAndItems
+                            {
+                                IdVistoria = reader.GetInt32(reader.GetOrdinal("IdVistoria")),
+                                Descricao = reader.GetString(reader.GetOrdinal("Descricao")),
+                                Processo = reader.GetString(reader.GetOrdinal("Processo")),
+                                Ativo = reader.GetString(reader.GetOrdinal("Ativo")),
+                                DataDeCadastro = reader.GetDateTime(reader.GetOrdinal("DataDeCadastro")),
+                                DhAlteracao = reader.GetDateTime(reader.GetOrdinal("DhAlteracao"))
+                            };
+
+                            if (joinedString != "null")
+                            {
+                                vistoria.Items = itensAndOptions;
+                            }
+
+                            vistorias.Add(vistoria);
+                        }
+                    }
+                }
+
+                _connection.Close();
+                _logService.PerformOperation("GET", "Dados de VISTORIAS com o mesmo id de vistoria retornados.");
+
+                return vistorias;
+            }
+
+            catch (Exception ex)
+            {
+                _logService.PerformOperation("GET", $"{ex.Message}");
+                throw new Exception(ex.Message);
+            }
+        }
+
 
         public VistoriaModel PostVistoria(VistoriaModel vistoriaModel)
         {
